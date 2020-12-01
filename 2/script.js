93d7ea7f747d8dd84bf095b49dd485b8448c0973
Dmitriy Diukov <diukovdmitriy@gmail.com>
2020-12-01 17:51:44 +0200

    second task done

---
+++ 2/script.js
@@ -0,0 +1,40 @@
+const randomizer = maxNumber => Math.floor(Math.random() * maxNumber);
+
+const askFirstNumber = () => +prompt('Введи число от 0 до 10');
+
+const askOtherNumber = () => +prompt('Попробуй еще раз');
+
+const guessFirstNumber = () => {
+  let answer = askFirstNumber();
+  
+  return answer === randomNumber;
+}
+
+const guessOtherNumber = () => {
+  let answer = askOtherNumber();
+
+  while (answer !== null && answer !== randomNumber) {
+    answer = askOtherNumber()
+  }
+
+  return answer === randomNumber;
+}
+
+const startGame = () => {
+  let result = guessFirstNumber();
+
+  if (result) {
+    alert('Надо же, угадал с первого раза!')
+  } else {
+    result = guessOtherNumber();
+
+    if (result) {
+      alert('Наконец ты выйграл!')
+    }
+  }
+}
+
+const randomNumber = randomizer(10);
+console.log(randomNumber);
+
+startGame ();
